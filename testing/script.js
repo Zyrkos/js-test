f251399f16174e0ee1c772625d4ae3d8a8043f96
Pedro Castro <106441980+Zyrkos@users.noreply.github.com>
2022-06-28 12:25:57 +0100

    Add files via upload

---
+++ testing/script.js
@@ -0,0 +1,23 @@
+
+
+/*function favoriteCars(arr) {
+const carBrands = ['Fiat', 'Opel', 'BMW', 'Audi', 'Mercedes'];
+const randCar = Math.floor(Math.random() * carBrands.length);
+const randomChoice = carBrands[randCar];
+return randomChoice;
+
+}
+
+randomChoice = favoriteCars();
+
+console.log (randomChoice); */
+
+
+
+const cats = ['Leopard', 'Serval', 'Jaguar', 'Tiger', 
+'Caracal', 'Lion']
+
+for(const cat of cats) {
+    console.log(cat);
+}
+
