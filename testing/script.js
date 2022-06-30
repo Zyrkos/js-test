8940e5cfa1d8469aa4ebdd96ea47f0f76523800b
Pedro Castro <106441980+Zyrkos@users.noreply.github.com>
2022-06-30 16:52:41 +0100

    Add files via upload

--- testing/script.js
+++ testing/script.js
@@ -1,23 +1,44 @@
 
 
-/*function favoriteCars(arr) {
-const carBrands = ['Fiat', 'Opel', 'BMW', 'Audi', 'Mercedes'];
-const randCar = Math.floor(Math.random() * carBrands.length);
-const randomChoice = carBrands[randCar];
-return randomChoice;
+ const container = document.querySelector('#container');
+const content = document.createElement('p');
+content.classList.add('paragraph');
+content.textContent = 'Hey I’m red!';
+content.style.color = 'red'
+container.appendChild(content);
 
-}
+const heading = document.createElement('h3');
+heading.classList.add('header');
+heading.textContent = 'I’m a blue h3!';
+heading.style.color = 'blue'
+container.appendChild(heading); 
 
-randomChoice = favoriteCars();
 
-console.log (randomChoice); */
+const division = document.createElement('div');
+division.classList.add('content');
+division.style.borderColor = 'black';
+division.style.backgroundColor = 'pink';
+const header = document.createElement('h1');
+header.textContent = 'I’m in a div';
+division.appendChild(header);
+const paragraph = document.createElement('p');
+paragraph.textContent = 'Me too!';
+paragraph.style.color = 'blue';
+division.appendChild(paragraph);
+
+container.appendChild(division);  
+
+
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert("Hello World");
 
 
 
-const cats = ['Leopard', 'Serval', 'Jaguar', 'Tiger', 
-'Caracal', 'Lion']
 
-for(const cat of cats) {
-    console.log(cat);
-}
+
+
+
+
+
+
 
